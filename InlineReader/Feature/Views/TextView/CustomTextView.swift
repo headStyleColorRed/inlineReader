f62badeefeadab7ca879d1bf555a45377568c8f4
Rodrigo Labrador <headstylecolorred@gmail.com>
2024-12-28 19:20:55 +0000

    annotating

--- InlineReader/Feature/Views/TextView/CustomTextView.swift
+++ InlineReader/Feature/Views/TextView/CustomTextView.swift
@@ -24,7 +24,7 @@ class CustomTextView: UITextView, UIEditMenuInteractionDelegate {
 
     // Enable custom actions
     override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
-        if action == #selector(customCopy(_:)) || action == #selector(translate(_:)) {
+        if action == #selector(customCopy(_:)) || action == #selector(translate(_:)) || action == #selector(annotate(_:)) {
             return true
         }
         return false
@@ -47,6 +47,18 @@ class CustomTextView: UITextView, UIEditMenuInteractionDelegate {
         print("Translate action performed.")
     }
 
+    // Annotation action
+    @objc func annotate(_ sender: Any?) {
+        print("Starting annotation action")
+        if let selectedRange = self.selectedTextRange {
+            let attributedText = NSMutableAttributedString(attributedString: self.attributedText)
+            let range = self.selectedRange
+            attributedText.addAttribute(.backgroundColor, value: UIColor.yellow, range: range)
+            self.attributedText = attributedText
+        }
+        print("Annotation action performed.")
+    }
+
     override func editMenu(for textRange: UITextRange, suggestedActions: [UIMenuElement]) -> UIMenu? {
         print("Creating edit menu for text range: \(textRange)")
         let customCopyAction = UIAction(title: "Copy", image: nil) { _ in
@@ -58,6 +70,12 @@ class CustomTextView: UITextView, UIEditMenuInteractionDelegate {
             print("Translate action selected from menu")
             self.translate(nil)
         }
-        return UIMenu(children: [customCopyAction, chatAction])
+
+        let annotateAction = UIAction(title: "Annotate", image: nil) { _ in
+            print("Annotate action selected from menu")
+            self.annotate(nil)
+        }
+
+        return UIMenu(children: [customCopyAction, chatAction, annotateAction])
     }
 }
