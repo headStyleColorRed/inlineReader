eefa4bd277df9bad174d105f7e8c50beff154d21
Rodrigo Labrador <headstylecolorred@gmail.com>
2025-01-05 19:29:12 +0000

    removed pdf support in Text reader

--- InlineReader/Feature/Views/ReaderView/TextReaderViewModel.swift
+++ InlineReader/Feature/Views/ReaderView/TextReaderViewModel.swift
@@ -1,5 +1,4 @@
 import SwiftUI
-import PDFKit
 
 class TextReaderViewModel: ObservableObject {
     @Published var text: NSAttributedString = NSAttributedString(string: "")
@@ -12,20 +11,12 @@ class TextReaderViewModel: ObservableObject {
 
     // MARK: - Public methods
     func viewDidLoad() {
-        print("file type: \(file.fileType)")
-        switch file.fileType {
-        case .text:
-            loadText(file: file)
-        case .pdf:
-            loadPDFText(file: file)
-        default:
-            break
-        }
+        loadText(file: file)
     }
 
 
     // MARK: - Private methods
-    func loadText(file: File) {
+    private func loadText(file: File) {
         do {
             guard let documentURL = file.fullURL else { throw "Document URL not found for file: \(file.name).txt" }
 
@@ -36,31 +27,7 @@ class TextReaderViewModel: ObservableObject {
                 self.text = NSAttributedString(string: fileContents)
             }
         } catch let error {
-            print(error)
-        }
-    }
-
-    func loadPDFText(file: File) {
-        do {
-            guard let documentURL = file.fullURL else { throw "Document URL not found for file: \(file.name).pdf" }
-            guard let document = PDFDocument(url: documentURL) else { throw "Failed to load PDF document." }
-
-            // Convert entire PDF to text
-            var fullText = ""
-            for pageIndex in 0..<document.pageCount {
-                guard let page = document.page(at: pageIndex) else { continue }
-                if let pageText = page.string {
-                    fullText += pageText + "\n\n"
-                }
-            }
-
-            guard !fullText.isEmpty else { throw "No text found in this document." }
-
-            DispatchQueue.main.async {
-                self.text = NSAttributedString(string: fullText)
-            }
-        } catch let error {
-            print(error)
+            BannerManager.showError(message: error.localizedDescription)
         }
     }
 }
